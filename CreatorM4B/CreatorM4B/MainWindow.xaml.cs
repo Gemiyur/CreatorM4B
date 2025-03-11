c597f3b17c6e39cd3ffa9d9ebba470496d990ccb
Геннадий <gemiyur@live.ru>
2025-03-11 17:18:34 +0300

    Добавил BackgroundWorker.

--- CreatorM4B/CreatorM4B/MainWindow.xaml.cs
+++ CreatorM4B/CreatorM4B/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,14 +19,60 @@ namespace CreatorM4B
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly BackgroundWorker Worker = new()
+        {
+            WorkerSupportsCancellation = true,
+            WorkerReportsProgress = true
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+            Worker.DoWork += Worker_DoWork;
+            Worker.ProgressChanged += Worker_ProgressChanged;
+            Worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
         }
 
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        private FileInfo? Merge(string folderName, string fileName, bool allImages, BackgroundWorker? worker, DoWorkEventArgs e)
         {
+            return null;
+        }
 
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if (Worker.IsBusy == true)
+            {
+                MessageBox.Show("Выполняется операция.", Title);
+                e.Cancel = true;
+            }
+        }
+
+        private void Worker_DoWork(object? sender, DoWorkEventArgs e)
+        {
+            var parameters = (object[]?)e.Argument;
+            BackgroundWorker? worker = sender as BackgroundWorker;
+            e.Result = parameters != null
+                ? Merge((string)parameters[0], (string)parameters[1], (bool)parameters[2], worker, e)
+                : null;
+        }
+
+        private void Worker_ProgressChanged(object? sender, ProgressChangedEventArgs e)
+        {
+            StatusTextBlock.Text = (string)e.UserState;
+        }
+
+        private void Worker_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+                StatusTextBlock.Text = "Ошибка";
+            }
+            else
+            {
+                StatusTextBlock.Text = "Готово";
+            }
+            var result = (FileInfo?)e.Result;
         }
 
         private void FolderButton_Click(object sender, RoutedEventArgs e)
@@ -33,7 +82,26 @@ namespace CreatorM4B
 
         private void CreateButton_Click(object sender, RoutedEventArgs e)
         {
+            if (Worker.IsBusy == true)
+            {
+                MessageBox.Show("Выполняется предыдущая операция.", Title);
+                return;
+            }
 
+            var fileDialog = new SaveFileDialog()
+            {
+                AddToRecent = false,
+                DefaultExt = ".m4b",
+                Title = "Создание файл книги",
+                Filter = $"Файлы книг|*.m4b"
+            };
+            if (fileDialog.ShowDialog() != true)
+                return;
+            var allImages = MessageBox.Show("Добавить изображения из всех файлов?", Title,
+                                             MessageBoxButton.YesNo) == MessageBoxResult.Yes;
+            object[] parameters = [FolderTextBox.Text, fileDialog.FileName, allImages];
+            StatusTextBlock.Text = "Создание...";
+            Worker.RunWorkerAsync(parameters);
         }
 
         private void AboutButton_Click(object sender, RoutedEventArgs e)
